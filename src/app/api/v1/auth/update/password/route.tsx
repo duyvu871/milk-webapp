eec4a9badc6eab8339f5ed8c2f570dd051417983
duyvu871 <zoocp123@gmail.com>
2023-12-30 00:47:19 +0700

    fix update password routing

---
+++ src/app/api/v1/auth/update/password/route.tsx
@@ -0,0 +1,11 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export async function POST(req: NextRequest) {
+    const {} = await req.json();
+    return new NextResponse(JSON.stringify({}), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+}
