d0cb5800d84ffa884b8611e402f499695566f956
duyvu871 <zoocp123@gmail.com>
2023-12-30 00:57:06 +0700

    change api to dev mode

--- src/configs/App.config.ts
+++ src/configs/App.config.ts
@@ -1,6 +1,6 @@
 const AppConfig = {
-    // mainApiUrl: 'https://ha-nam-milk-backend-s1.onrender.com',
-    mainApiUrl: 'http://localhost:3002',
+    mainApiUrl: 'https://ha-nam-milk-backend-s1.onrender.com',
+    // mainApiUrl: 'http://localhost:3002',
 }
 
 export default AppConfig;
